73b6fd44795c382924093863b7157cfd9dc85abd
Niklas Kemper <niklas.kemper@tum.de>
2022-11-22 18:40:01 +0100

    Create own python module for R backend
    Encapsulate all parts of the code handling R code in own class and
    module. With this, there is a clear seperation of pure python code
    and a backend handling R.
    
    Additionally, add a backend class for the fitted mgcv GAM.
    
    Furthermore, add multiple docstrings.
    
    Resolves: #24

---
+++ tradeseq/gam/_backend.py
@@ -0,0 +1,116 @@
+from rpy2.robjects import numpy2ri, default_converter
+from rpy2.robjects.packages import importr
+from rpy2.robjects.conversion import localconverter
+import rpy2.robjects as ro
+
+import numpy as np
+
+
+class GAM:
+    """GAM backend class encapsulating R gam object."""
+
+    def __init__(self, gam):
+        """
+        Initialize GAM object.
+
+        Parmaeters
+        ----------
+        gam:
+            rpy2 representation of fitted mgcv GAM object.
+        """
+        self._gam = gam
+
+    def predict(self):
+        """TODO."""
+
+
+class GAM_Fitting:
+    """Backend class used for fitting GAMs in R for multiple genes."""
+
+    def __init__(self, pseudotimes: np.ndarray, w_sample: np.ndarray, knots: np.ndarray, smooth_form: str, family: str):
+        """Initialize class and assing pseudotime and cell_weight values to corresponding variales in R.
+
+        Parameters
+        ----------
+        pseudotimes
+            A `n_cells`` x ``n_lineage`` np.ndarray containing pseudotimes for every cell and lineage.
+        w_sample
+            A ``n_cells`` x ``n_lineage`` np.ndarray where each row contains exactly one 1 (the assigned lineage).
+            and 0 everywhere else
+        knots
+            Location of knots used for fitting the splines in the GAM.
+        smooth_form
+            Smooth form of the fitted GAM. Can be any formula accepted by mgcv.gam where y stands for gene expression
+            data, and l_{lineage_id}, t_{lineage_id} are variables for the lineage-assignment and the pseudotime values,
+            respectively.
+        family
+            Family of probability distributions that is used for fitting the GAM. Defaults to the negative binomial
+            distributions. Can be any family available in mgcv.gam.
+        """
+        _assign_pseudotimes(pseudotimes)
+        _assign_lineages(w_sample)
+        self._knots = [float(knot) for knot in knots]  # Convert to list to make conversion to R easier
+        self._smooth_form = smooth_form
+        self._family = family
+        self._mgcv = importr("mgcv")
+
+    def fit_gam(self, y: np.ndarray) -> GAM:
+        """Fit GAM for a single gene.
+
+        Parameters
+        ----------
+        y
+            A np.ndarray of shape (``n_cells``,) containing gene expression data for a single gene.
+
+        Returns
+            Fitted GAM object.
+        """
+        ro.globalenv["n_knots"] = len(self._knots)
+        ro.globalenv["y"] = ro.vectors.FloatVector(y)
+        gam = self._mgcv.gam(ro.Formula(self._smooth_form), family=self._family, knots=self._knots)
+        return GAM(gam)
+
+
+def _assign_pseudotimes(pseudotimes: np.ndarray):
+    """Assign pseudotimes in R.
+
+    Assign pseudotimes for every lineage to variables t_{lineage_id} in R.
+
+    Parameters
+    ----------
+    pseudotimes
+        A `n_cells`` x ``n_lineage`` np.ndarray containing pseudotimes for every cell and lineage.
+    """
+    np_cv_rules = default_converter + numpy2ri.converter
+    with localconverter(np_cv_rules):
+        ro.globalenv["pseudotimes"] = pseudotimes
+    ro.r(
+        """
+    for (lineage_id in seq_len(ncol(pseudotimes))) {
+        assign(paste0("t",lineage_id), pseudotimes[,lineage_id])
+    }
+    """
+    )
+
+
+def _assign_lineages(w_sample: np.ndarray):
+    """Assign lineage assingments in R.
+
+    Assign lineage indicators for every lineage to variables l_{lineage_id} in R.
+
+    Parameters
+    ----------
+    w_sample
+        A ``n_cells`` x ``n_lineage`` np.ndarray where each row contains exactly one 1 (the assigned lineage).
+        and 0 everywhere else
+    """
+    np_cv_rules = default_converter + numpy2ri.converter
+    with localconverter(np_cv_rules):
+        ro.globalenv["w_sample"] = w_sample
+    ro.r(
+        """
+    for (lineage_id in seq_len(ncol(w_sample))) {
+        assign(paste0("l", lineage_id), 1 * (w_sample[, lineage_id] == 1))
+    }
+    """
+    )
--- tradeseq/gam/fitGAM.py
+++ tradeseq/gam/fitGAM.py
@@ -1,83 +1,97 @@
-from anndata import AnnData
-import numpy as np
-import warnings
-import pandas as pd
 from typing import Tuple, Optional
+import warnings
+
+from anndata import AnnData
 from scipy.sparse import issparse
-import rpy2.robjects as ro
-from rpy2.robjects import numpy2ri
-from rpy2.robjects import default_converter
-from rpy2.robjects.packages import importr
-from rpy2.robjects.conversion import localconverter
+import _backend
+
+import numpy as np
+import pandas as pd
 
 
 class GAM:
+    """Class for fitting and working with GAMs for gene expression data.
+
+    First, the fit method should be called to fit GAMs for (a subset of) genes.
+    Then, one can make predictions with and plot the fitted GAMs.
+    """
+
     def __init__(self, adata: AnnData):
-        self._adata = adata
-        self._model = None
+        """
+        Initialize GAM class.
+
+        Parameters
+        ----------
+        adata: AnnData object containing the gene counts, the cell to lineage weigths and the pseudotimes
+        """
+        self._adata: AnnData = adata
+        self._model: Optional[list[_backend.GAM]] = None
         self._genes = None
         self._knots: Optional[np.ndarray] = None
 
     def predict(self):
+        """TODO."""
         pass  # TODO: add method for predicting with fitted GAMs
 
     def _get_pseudotime(self, time_key: str, n_lineages: int) -> np.ndarray:
-        """
-        Retrieves pseudotime from ``self._adata``
+        """Retrieve pseudotime from ``self._adata``.
 
         Parameters
         ----------
-        time_key:
+        time_key
             Key for pseudotime values,
             ``self._adata`` has to contain pseudotime values for every lineage
-            in ``adata.obsm[time_key]`` or ``adata.obs[time_key]`
-        n_lineages:
+            in ``adata.obsm[time_key]`` or ``adata.obs[time_key]``
+        n_lineages
             Number of lineages
 
         Returns
         --------
         Array of shape (``self._adata.n_obs``,``_lineages``) containing pseudotime values for all lineages
         """
-
         if time_key in self._adata.obs.columns:
             pseudotime = self._adata.obs[time_key].values
         elif time_key in self._adata.obsm.keys:
             pseudotime = np.array(self._adata.obsm[time_key])
         else:
-            raise KeyError(f"Invalid key {time_key} for pseudotimes."
-                           f"The key {time_key} must be either in adata.obs or adata.obsm")
+            raise KeyError(
+                f"Invalid key {time_key} for pseudotimes."
+                f"The key {time_key} must be either in adata.obs or adata.obsm"
+            )
 
         if pseudotime.ndim == 1:
             return np.repeat(pseudotime[:, None], n_lineages, axis=1)
 
         if pseudotime.shape != (self._adata.n_obs, n_lineages):
-            raise ValueError("Invalid pseudotime shape.\n"
-                             f"Expected shape ({self._adata.n_obs}, {n_lineages}) or ({self._adata.n_obs},)\n"
-                             f"Actual shape: {pseudotime.shape}")
+            raise ValueError(
+                "Invalid pseudotime shape.\n"
+                f"Expected shape ({self._adata.n_obs}, {n_lineages}) or ({self._adata.n_obs},)\n"
+                f"Actual shape: {pseudotime.shape}"
+            )
 
         return pseudotime
 
-    def _get_lineage(self, lineage_key: str) -> Tuple[np.ndarray, list]:
-        """
-        Retrieves cell to lineage weights from ``self._adata``
+    def _get_lineage(self, weights_key: str) -> Tuple[np.ndarray, list]:
+        """Retrieve cell to lineage weights from ``self._adata``.
 
         Parameters
         ----------
-        lineage_key
+        weights_key
             Key for cell to lineage weights,
             ``self._adata`` has to contain a weights object of shape (``self._adata.n_obs``, n_lineages)
-            in ``adata.obsm[lineage_key]``
+            in ``adata.obsm[weights_key]``
 
         Returns
         -------
             Tuple of cell to lineage weights as a (``self._adata.n_obs``, n_lineages) array and lineage names
         """
-
         try:
-            data = self._adata.obsm[lineage_key]
+            data = self._adata.obsm[weights_key]
         except KeyError:
-            raise KeyError(f"Invalid key {lineage_key} for cell to lineage assignment."
-                           f"The key {lineage_key} must be present in adata.obsm")
+            raise KeyError(
+                f"Invalid key {weights_key} for cell to lineage assignment."
+                f"The key {weights_key} must be present in adata.obsm"
+            )
 
         if isinstance(data, pd.DataFrame):
             weights = data.values
@@ -93,13 +107,13 @@ class GAM:
             raise (
                 f"Invalid cell weight shape.\n"
                 f"Expected shape: ({self._adata.n_obs}, n_lineages) or ({self._adata.n_obs},).\n"
-                f"Actual shape: {data.shape}")
+                f"Actual shape: {data.shape}"
+            )
 
         return weights, names
 
     def _get_knots(self, time_key: str, n_lineages: int, n_knots: int) -> np.ndarray:
-        """
-        Calculates knot locations at quantiles of pseudotime values (of all lineages).
+        """Calculate knot locations at quantiles of pseudotime values (of all lineages).
 
         If possible, end points of lineages are used as knots.
         Knot locations are stored in ``adata.uns[tradeSeq.knots]`` and returned.
@@ -148,20 +162,21 @@ class GAM:
         if np.unique(replace_ids).size < np.unique(end_points).size:
             warnings.warn(
                 "Impossible to place a knot at all endpoints. Increase the number of knots to avoid this issue",
-                RuntimeWarning)
+                RuntimeWarning,
+            )
         knots[replace_ids] = end_points
         self._knots = knots
         return knots
 
-    def _assign_cells_to_lineages(self, weights_key: str):
-        """Assigns every cell randomly to one lineage with probabilities based on the supplied lineage weights.
+    def _assign_cells_to_lineages(self, weights_key: str) -> np.ndarray:
+        """Assign every cell randomly to one lineage with probabilities based on the supplied lineage weights.
 
         Parameters
         ----------
-        adata
-            AnnData object which contains lineage weights for every cell and lineage in ``adata.obsm[weights_key]``
         weights_key
-            Key for weights
+            Key for cell to lineage weights,
+            ``self._adata`` has to contain a weights object of shape (``self._adata.n_obs``, n_lineages)
+            in ``adata.obsm[weights_key]``
 
         Returns
         -------
@@ -171,7 +186,8 @@ class GAM:
         cell_weights, _ = self._get_lineage(weights_key)
         if not _check_cell_weights(cell_weights):
             raise ValueError(
-                "Cell weights have to be non-negative and cells need to have at least one positive cell weight")
+                "Cell weights have to be non-negative and cells need to have at least one positive cell weight"
+            )
 
         def sample_lineage(cell_weights_row):
             return np.random.multinomial(1, cell_weights_row / np.sum(cell_weights_row))
@@ -179,11 +195,11 @@ class GAM:
         return np.apply_along_axis(sample_lineage, 1, cell_weights)  # TODO: compare with jit
 
     def _get_counts(
-            self,
-            layer_key: Optional[str] = None,
-            use_raw: bool = False,
-    ) -> Tuple[np.ndarray, np.ndarray]:
-        """Retrieves gene expression counts from ``self._adata``.
+        self,
+        layer_key: Optional[str] = None,
+        use_raw: bool = False,
+    ) -> Tuple[np.ndarray, list]:
+        """Retrieve gene expression counts from ``self._adata``.
 
         Parameters
         ----------
@@ -205,7 +221,7 @@ class GAM:
             data = self._adata.raw
         else:
             data = self._adata
-        genes = list([f"{name}" for name in self._adata.var_names])
+        genes = [f"{name}" for name in self._adata.var_names]
 
         if layer_key is None:
             counts = data.X
@@ -217,59 +233,72 @@ class GAM:
         # TODO(michalk): warn of too many genes
         return (counts.A if issparse(counts) else counts), genes
 
-    def fit(self, layer_key: str, weights_key: str, pseudotime_key: str, offset_key: str, genes,
-                n_jobs: Optional[int],
-                family: str = "nb", n_knots: int = 6, verbose: bool = True):
+    def fit(
+        self,
+        layer_key: str,
+        weights_key: str,
+        time_key: str,
+        offset_key: str,
+        genes,
+        n_jobs: Optional[int],
+        family: str = "nb",
+        n_knots: int = 6,
+        verbose: bool = True,
+    ):
+        """Fit generalized additive model for every selected gene.
 
+        The GAMs all share the same knot locations for the spline functions. For every lineage and gene an own spline
+        function is fitted. The cells are assigned to lineages randomly according to the given cell weights.
+        TODO: add details to docstring
+
+        Parameters
+        ----------
+        layer_key
+            Key for the layer from which to retrieve the counts in ``self._adata`` If None, ``self._adata.X`` is used.
+        weights_key
+            Key for cell to lineage weights,
+            ``self._adata`` has to contain a weights object of shape (``self._adata.n_obs``, n_lineages)
+            in ``adata.obsm[weights_key]``.
+        time_key
+            Key for pseudotime values,
+            ``self._adata`` has to contain pseudotime values for every lineage
+            in ``adata.obsm[time_key]`` or ``adata.obs[time_key]``.
+        offset
+            TODO
+        genes
+            TODO
+        n_jobs:
+            TODO
+        family
+            Family of probability distributions that is used for fitting the GAM. Defaults to the negative binomial
+            distributions. Can be any family available in mgcv.gam.
+        n_knots
+            Number of knots that are used for the splines in the GAM.
+        verbose
+            TODO
+        """
         w_sample = self._assign_cells_to_lineages(weights_key)
         n_lineages = w_sample.shape[1]
-        knots = self._get_knots(pseudotime_key, n_lineages, n_knots)
-        pseudotimes = self._get_pseudotime(pseudotime_key, n_lineages)
-
-        np_cv_rules = default_converter + numpy2ri.converter
-        with localconverter(np_cv_rules):
-            ro.globalenv["pseudotimes"] = pseudotimes
-            ro.globalenv["w_sample"] = w_sample
-
-        # define pseudotimes t{ii} in R for every lineage
-        ro.r('''
-        for (ii in seq_len(ncol(pseudotimes))) {
-            assign(paste0("t",ii), pseudotimes[,ii])
-        }
-        ''')
-
-        # define lineage indicators l{ii} in R for every lineage
-        ro.r('''
-        for (ii in seq_len(ncol(pseudotimes))) {
-            assign(paste0("l", ii), 1 * (w_sample[, ii] == 1))
-        }
-        ''')
+        knots = self._get_knots(time_key, n_lineages, n_knots)
+        pseudotimes = self._get_pseudotime(time_key, n_lineages)
 
         right_side = "+".join(
-            [f"s(t{i}, by=l{i}, bs='cr', id=1, k=n_knots)" for i in range(1,n_lineages+1)])  # TODO: add offset
+            [f"s(t{i}, by=l{i}, bs='cr', id=1, k=n_knots)" for i in range(1, n_lineages + 1)]
+        )  # TODO: add offset
         smooth_form = "y ~ " + right_side
 
-        mgcv = importr("mgcv")
-
-        def fit_gam_for_gene(y):
-            ro.globalenv["n_knots"] = n_knots
-            ro.globalenv["y"] = ro.vectors.FloatVector(y)
-            knots_l = [float(i) for i in knots]
-            gam = mgcv.gam(ro.Formula(smooth_form), family=family, knots=knots_l)
-            return gam
+        backend = _backend.GAM_Fitting(pseudotimes, w_sample, knots, smooth_form, family)
 
         use_raw = False
         counts, gene_names = self._get_counts(layer_key, use_raw)
         gams = []
         for gene_count in counts.T:
-            gams.append(fit_gam_for_gene(gene_count))
+            gams.append(backend.fit_gam(y=gene_count))
         self._model = gams
-        return gams
 
 
 def _check_cell_weights(cell_weights: np.ndarray) -> bool:
-    """
-    Checks if all cell weights are non-negative and if every cell has at least one positive cell weight
+    """Check if all cell weights are non-negative and if every cell has at least one positive cell weight.
 
     Parameters
     __________
@@ -278,19 +307,7 @@ def _check_cell_weights(cell_weights: np.ndarray) -> bool:
 
     Returns
     ________
-        Boolean indicating whether all cell weights are non-negative and if every cell has at least one positive cell weight
+        Boolean indicating whether all cell weights are non-negative and if every cell has at least one positive
+        cell weight.
     """
     return (cell_weights >= 0).all() and (np.sum(cell_weights, axis=1) > 0).all()
-
-
-
-
-
-
-
-
-
-
-
-
-
