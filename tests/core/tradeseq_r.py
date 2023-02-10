5cbcbad5a4b0417f528ad59b572029411beb5b44
Niklas Kemper <niklas.kemper@tum.de>
2023-02-10 17:07:18 +0100

    Add doctring.

--- tests/core/tradeseq_r.py
+++ tests/core/tradeseq_r.py
@@ -8,6 +8,10 @@ from tradeseq.gam._backend import GAM
 
 
 class TradeseqR:
+    """
+    Bridge to tradeseq R implementation.
+    """
+
     def __init__(self, counts: np.ndarray, pseudotime: np.ndarray, cell_weights: np.ndarray, n_knots: int):
         np_cv_rules = default_converter + numpy2ri.converter
         with localconverter(np_cv_rules):
