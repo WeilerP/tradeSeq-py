5f327f86a278c1b62ae9b4d3f3dbe77295a012b7
Niklas Kemper <niklas.kemper@tum.de>
2023-02-12 13:32:28 +0100

    Restructer base class for de_tests.
    The base class now also contains the wald test. The wald_test.py file is
    deleted.

--- tradeseq/de_tests/_base.py
+++ tradeseq/de_tests/_base.py
@@ -1,9 +1,12 @@
 from abc import ABC, abstractmethod
+from typing import Literal
 
+from scipy.stats import chi2
+
+import numpy as np
 import pandas as pd
 
 from tradeseq.gam import GAM
-from tradeseq.de_tests.wald_test import wald_test
 
 
 class WithinLineageTest(ABC):
@@ -57,3 +60,34 @@ class WithinLineageTest(ABC):
         return pd.DataFrame.from_dict(
             result, orient="index", columns=["wald statistic", "degrees of freedom", "p value"]
         )
+
+
+def wald_test(
+    prediction: np.ndarray, contrast: np.ndarray, sigma: np.ndarray, inverse: Literal["cholesky", "eigen"] = "eigen"
+):
+    """Perform a wald test for the null hypothesis: contrast * fitted_parameters = 0.
+
+    Computes wald statistics: prediction (contrast sigma contrast^T)^(-1) prediction^T and the corresponding p value.
+
+    Parameter
+    ---------
+    prediction
+        A (`n_prediction`,) np.ndarray typically containing the difference between two predictions.
+    contrast
+        A (`n_prediction`, `n_params`) np.ndarray typically containing the difference between the lp matrices of the
+        same predictions as above.
+    sigma
+        A (`n_params`, `n_params`) np.ndarray, the covariance matrix for the fitted parameters.
+
+    Returns
+    -------
+    A tuple containing the wald statistic, the degrees of freedom and the p value.
+    """
+    sigma_inv = np.linalg.inv(contrast @ sigma @ contrast.T)
+    wald = prediction @ sigma_inv @ prediction.T
+    if wald < 0:
+        wald = 0
+
+    df = prediction.shape[0]
+    pval = 1 - chi2.cdf(wald, df)
+    return wald, df, pval
