Fix computation of the global test for between lineage tests.
The global tests compares all pairs of lineages simultaneously.
This was not the case in the previous version and is now fixed.

Additionally minor updates to the doc strings.

tradeseq/de_tests/_base.py
@@ -160,19 +160,28 @@ class BetweenLineageTest(DifferentialExpressionTest):
                 predictions.append(self._model.predict(gene_id, lineage_array, pseudotime, log_scale=True))
                 lpmatrices.append(self._model.get_lpmatrix(gene_id, lineage_array, pseudotime))
 
+            predictions_comb = [
+                predictions[lineage_a] - predictions[lineage_b] for (lineage_a, lineage_b) in combinations(lineages, 2)
+            ]
+            lpmatrices_comb = [
+                lpmatrices[lineage_a] - lpmatrices[lineage_b] for (lineage_a, lineage_b) in combinations(lineages, 2)
+            ]
+
             if pairwise_test:
-                for (lineage_a, lineage_b) in combinations(lineages, 2):
+                for prediction_diff, lpmatrix_diff, (lineage_a, lineage_b) in zip(
+                    predictions_comb, lpmatrices_comb, combinations(lineages, 2)
+                ):
                     result[
                         f"{gene_name} between lineages {self._model.lineage_names[lineage_a]} and {self._model.lineage_names[lineage_b]}"
                     ] = _wald_test(
-                        predictions[lineage_a] - predictions[lineage_b],
-                        lpmatrices[lineage_a] - lpmatrices[lineage_b],
+                        prediction_diff,
+                        lpmatrix_diff,
                         sigma,
                     )
 
             if global_test:
-                pred = np.concatenate(predictions)
-                lpmatrix = np.concatenate(lpmatrices, axis=0)
+                pred = np.concatenate(predictions_comb)
+                lpmatrix = np.concatenate(lpmatrices_comb, axis=0)
                 result[f"{gene_name} globally"] = _wald_test(pred, lpmatrix, sigma)
 
         return pd.DataFrame.from_dict(

tradeseq/de_tests/_diff_end_test.py
@@ -6,7 +6,7 @@ from tradeseq.de_tests._base import BetweenLineageTest
 class DiffEndTest(BetweenLineageTest):
     """The DiffEndTest checks whether a gene is expressed differently at the end of two lineages."""
 
-    def __call__(self, pairwise_test, global_test):
+    def __call__(self, pairwise_test: bool, global_test: bool):
         """
         Perform DiffEndTest.
 
@@ -22,7 +22,7 @@ class DiffEndTest(BetweenLineageTest):
         A Pandas DataFrame containing the Wald statistic, the degrees of freedom and the p-value
         for each gene for each pair of lineages (if ``pairwise_test``) and/or globally (if ``global_test``).
         """
-        end_pseudotimes = [np.ndarry([end]) for end in self._get_end_pseudotime()]
+        end_pseudotimes = [np.array([end]) for end in self._get_end_pseudotime()]
         lineages = np.arange(self._model._n_lineages)
 
         return self._test(end_pseudotimes, lineages, pairwise_test, global_test)